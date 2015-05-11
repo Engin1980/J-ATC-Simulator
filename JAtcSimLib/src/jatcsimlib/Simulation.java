e42dcaf7647d13a0c24d6a1eb4a10f457f475590
Engin <none@none>
2015-05-11 18:26:27 +0200

    Bug - airplanes separation 1 FL was not enough.
    Removed unused methods.

--- JAtcSimLib/src/jatcsimlib/Simulation.java
+++ JAtcSimLib/src/jatcsimlib/Simulation.java
@@ -66,7 +66,7 @@ public class Simulation {
       }
     }
   });
-
+  public static final ERandom rnd = new ERandom();
   private final ETime now;
   private final AirplaneTypes planeTypes;
   private final Airport airport;
@@ -76,9 +76,9 @@ public class Simulation {
   private final TowerAtc twrAtc;
   private final CenterAtc ctrAtc;
 
-  private int TRAFFIC_COUNT = 10;
+  private final int TRAFFIC_COUNT = 5;
   private final Traffic traffic = new GeneratedTraffic(
-    7, 1.0, new int[]{
+    7, 0.0, new int[]{
       TRAFFIC_COUNT, TRAFFIC_COUNT, TRAFFIC_COUNT, TRAFFIC_COUNT, TRAFFIC_COUNT, TRAFFIC_COUNT,
       TRAFFIC_COUNT, TRAFFIC_COUNT, TRAFFIC_COUNT, TRAFFIC_COUNT, TRAFFIC_COUNT, TRAFFIC_COUNT,
       TRAFFIC_COUNT, TRAFFIC_COUNT, TRAFFIC_COUNT, TRAFFIC_COUNT, TRAFFIC_COUNT, TRAFFIC_COUNT,
@@ -215,154 +215,6 @@ public class Simulation {
     return Acc.prm().getResponsibleAtc(plane);
   }
 
-  private Airplane generateNewArrivingPlane() {
-    Airplane ret;
-
-    Callsign cs = generateCallsign();
-    AirplaneType pt = planeTypes.getRandomByTraffic(Acc.airport().getTrafficCategories());
-
-    Route r = tryGetRandomRoute(true, pt);
-    if (r == null) {
-      r = tryGeneratePointRoute(true);
-    }
-    Coordinate coord = generateArrivalCoordinate(r.getMainFix().getCoordinate(), Acc.threshold().getCoordinate());
-    Squawk sqwk = generateSqwk();
-
-    int heading = (int) Coordinates.getBearing(coord, r.getMainFix().getCoordinate());
-    int alt = generateArrivingPlaneAltitude(r);
-    int spd = pt.vCruise;
-
-    List<Command> routeCmds = r.getCommandsListClone();
-    // added command to descend
-    routeCmds.add(0,
-      new ChangeAltitudeCommand(
-        ChangeAltitudeCommand.eDirection.descend,
-        Acc.atcCtr().getOrderedAltitude()
-      ));
-    // added command to contact CTR
-    routeCmds.add(0, new ContactCommand(Atc.eType.ctr));
-
-    ret = new Airplane(
-      cs, coord, sqwk, pt, heading, alt, spd, false,
-      r.getName(), routeCmds);
-
-    return ret;
-  }
-
-  private Airplane generateNewDepartingPlane() {
-    Airplane ret;
-
-    Callsign cs = generateCallsign();
-    AirplaneType pt = planeTypes.get(rnd.nextInt(planeTypes.size()));
-
-    Route r = tryGetRandomRoute(false, pt);
-    Coordinate coord = Acc.threshold().getCoordinate();
-    Squawk sqwk = generateSqwk();
-
-    int heading = (int) Acc.threshold().getCourse();
-    int alt = Acc.threshold().getParent().getParent().getAltitude();
-    int spd = 0;
-
-    List<Command> routeCmds = r.getCommandsListClone();
-
-    int indx = 0;
-    // added command to contact after departure
-    routeCmds.add(indx++, new ContactCommand(Atc.eType.twr));
-
-    routeCmds.add(indx++, new ChangeAltitudeCommand(
-      ChangeAltitudeCommand.eDirection.climb, Acc.threshold().getInitialDepartureAltitude()));
-
-    // -- po vysce+300 ma kontaktovat APP
-    routeCmds.add(indx++,
-      new AfterAltitudeCommand(Acc.threshold().getParent().getParent().getAltitude() + Acc.rnd().nextInt(150, 450)));
-    routeCmds.add(indx++, new ContactCommand(Atc.eType.app));
-
-    // -- po vysce + 3000 rychlost na odlet
-//    routeCmds.add(indx++,
-//        new AfterAltitudeCommand(Acc.threshold().getParent().getParent().getAltitude() + 3000));
-//    routeCmds.add(indx++, new ChangeSpeedCommand(ChangeSpeedCommand.eDirection.increase, 250));
-    ret = new Airplane(
-      cs, coord, sqwk, pt, heading, alt, spd, true,
-      r.getName(), routeCmds);
-
-    return ret;
-  }
-
-  //todel
-  private Squawk generateSqwk() {
-    int len = 4;
-    char[] tmp;
-    Squawk ret = null;
-    while (ret == null) {
-      tmp = new char[len];
-      for (int i = 0; i < len; i++) {
-        tmp[i] = Integer.toString(rnd.nextInt(8)).charAt(0);
-      }
-      ret = Squawk.create(tmp);
-      for (Airplane p : Acc.planes()) {
-        if (p.getSqwk().equals(ret)) {
-          ret = null;
-          break;
-        }
-      }
-    }
-    return ret;
-  }
-
-  //todel
-  private Coordinate generateArrivalCoordinate(Coordinate navFix, Coordinate aipFix) {
-    double radial = Coordinates.getBearing(aipFix, navFix);
-    radial += rnd.nextDouble() * 50 - 25; // nahodne zatoceni priletoveho radialu
-    double dist = rnd.nextDouble() * Global.MAX_ARRIVING_PLANE_DISTANCE; // vzdalenost od prvniho bodu STARu
-    Coordinate ret = null;
-    while (ret == null) {
-
-      ret = Coordinates.getCoordinate(navFix, (int) radial, dist);
-      for (Airplane p : Acc.planes()) {
-        double delta = Coordinates.getDistanceInNM(ret, p.getCoordinate());
-        if (delta < 5d) {
-          ret = null;
-          break;
-        }
-      }
-      dist += rnd.nextDouble() * 10;
-    }
-    return ret;
-  }
-
-  //todel
-  private Callsign generateCallsign() {
-    Callsign ret = null;
-    while (ret == null) {
-      ret = new Callsign("CSA", String.format("%04d", rnd.nextInt(10000)));
-      for (Airplane p : Acc.planes()) {
-        if (ret.equals(p.getCallsign())) {
-          ret = null;
-          break;
-        }
-      }
-    }
-    return ret;
-  }
-
-  public static final ERandom rnd = new ERandom();
-// todel
-
-  private Route tryGetRandomRoute(boolean arrival, AirplaneType planeType) {
-
-    Iterable<Route> rts = Acc.threshold().getRoutes();
-    List<Route> avails = Routes.getByFilter(rts, arrival, planeType.category);
-
-    if (avails.isEmpty()) {
-      return null; // if no route, return null
-    }
-    int index = rnd.nextInt(avails.size());
-
-    Route ret = avails.get(index);
-
-    return ret;
-  }
-
   public Weather getWeather() {
     return weather;
   }
@@ -379,31 +231,17 @@ public class Simulation {
     return ctrAtc;
   }
 
-  //todel
-  private int generateArrivingPlaneAltitude(Route r) {
-    double thousandsFeetPerMile = 0.30;
-
-    double dist = r.getRouteLength();
-    if (dist < 0) {
-      dist = Coordinates.getDistanceInNM(r.getMainFix().getCoordinate(), Acc.airport().getLocation());
-    }
-
-    int ret = (int) (dist * thousandsFeetPerMile) + rnd.nextInt(5, 12);
-    ret = ret * 1000;
-    return ret;
-  }
-
   private void removeOldPlanes() {
     AirplaneList rem = new AirplaneList();
     for (Airplane p : Acc.planes()) {
       // landed
-      if (p.isArrival() && p.getSpeed() < 30) {
+      if (p.isArrival() && p.getSpeed() < 11) {
         rem.add(p);
       }
 
       // departed
       if (p.isDeparture() && Acc.prm().getResponsibleAtc(p).equals(Acc.atcCtr())
-        && (p.getAltitude() == p.getTargetAltitude() || p.getAltitude() > 18000)) {
+        && (p.getAltitude() == p.getTargetAltitude() || p.getAltitude() > Acc.atcCtr().getReleaseAltitude() + 2000)) {
         rem.add(p);
       }
     }
@@ -423,38 +261,6 @@ public class Simulation {
 
   public RunwayThreshold getActiveRunwayThreshold() {
     return Acc.threshold();
-  }
-
-  //todel
-  private Route tryGeneratePointRoute(boolean arrival) {
-    //1. take points from arriving routes
-    List<Navaid> nvs = new LinkedList();
-
-    for (Runway rw : Acc.airport().getRunways()) {
-      for (RunwayThreshold rt : rw.getThresholds()) {
-        for (Route r : rt.getRoutes()) {
-          if (arrival && r.getType() == Route.eType.sid) {
-            continue;
-          } else if (!arrival && r.getType() != Route.eType.sid) {
-            continue;
-          }
-
-          Navaid n = r.getMainFix();
-
-          if (nvs.contains(n) == false) {
-            nvs.add(n);
-          }
-        }
-      }
-    }
-
-    int index = Acc.rnd().nextInt(nvs.size());
-
-    Navaid n = nvs.get(index);
-
-    Route r = Route.createNewByFix(n, arrival);
-
-    return r;
   }
 
   private void processSystemMessages() {
