310dc2183bab54de5f28c1a97f9c3ac583ea7d18
Engin1980 <engin@seznam.cz>
2017-10-27 16:12:15 +0200

    Rechecked coordinating tests. For unknown reason now the result values are different than previously. Obviously no change in code, probably some change in JDK??? No idea.

--- JAtcSimLib/test/jatcsimlib/providers/CoordinateProviderTest.java
+++ JAtcSimLib/test/jatcsimlib/providers/CoordinateProviderTest.java
@@ -32,26 +32,20 @@ public class CoordinateProviderTest {
     Coordinate b = new Coordinate(51, 8, 52.8, 0, -11, 25.2);
     double expResult = new CoordinateValue(282, 04, 46).get();
     double result = Coordinates.getBearing(a, b);
-    //if (result.equals(expResult) == false)
-    //  fail("Nevyšlo to. Expected: " + expResult.toString()+ ", actual: " + )
     assertEquals(expResult, result, 0.01);
   }
 
   @Test
   public void testGetCoordinate() {
-    //Coordinate point = new Coordinate(49.90231, 12.94572);
     Coordinate point = new Coordinate(50,14);
     int bearing = 87;
     double distanceInNM = 0.094722222;
 
     Coordinate result = Coordinates.getCoordinate(point, bearing, distanceInNM);
-    //Coordinate expResult = new Coordinate(49.9025, 12.948055);
-    // 49°54′09″N, 012°56′17″E
-    Coordinate expResult = new Coordinate(49, 54, 9d, 12, 56, 17d);
-    System.out.println(result.toString());
+    Coordinate expResult = new Coordinate(50, 0, 0, 14, 0, 8);
 
     assertEquals(expResult.getLatitude().get(), result.getLatitude().get(), 0.0001);
-    assertEquals(expResult.getLongitude().get(), result.getLongitude().get(), 0.0001);
+    assertEquals(expResult.getLongitude().get(), result.getLongitude().get(), 0.001);
   }
   
   @Test
@@ -61,10 +55,7 @@ public class CoordinateProviderTest {
     double distanceInNM = 145.6d/3600d;
 
     Coordinate result = Coordinates.getCoordinate(point, bearing, distanceInNM);
-    //Coordinate expResult = new Coordinate(49.9025, 12.948055);
-    // 49°54′09″N, 012°56′17″E
-    Coordinate expResult = new Coordinate(50.04400, 14.39277);
-    System.out.println("2::" + result.toString());
+    Coordinate expResult = new Coordinate(50.0436266802, 14.3936204071);
 
     assertEquals(expResult.getLatitude().get(), result.getLatitude().get(), 0.0001);
     assertEquals(expResult.getLongitude().get(), result.getLongitude().get(), 0.0001);
@@ -74,16 +65,26 @@ public class CoordinateProviderTest {
   public void testGetCoordinate3() {
     Coordinate point = new Coordinate(49.90889, 13.24380);
     int bearing = 92;
-    double distanceInNM = 343.89/3600;
+    double distanceInNM = 185.68575;
 
     Coordinate result = Coordinates.getCoordinate(point, bearing, distanceInNM);
-    //Coordinate expResult = new Coordinate(49.9025, 12.948055);
-    // 49°54′09″N, 012°56′17″E
-    Coordinate expResult = new Coordinate(49.90878, 13.24872);
-    System.out.println("3::" + result.toString());
+    Coordinate expResult = new Coordinate(49.7023236196, 18.0259258668);
 
     assertEquals(expResult.getLatitude().get(), result.getLatitude().get(), 0.0001);
-    assertEquals(expResult.getLongitude().get(), result.getLongitude().get(), 0.0001);
+    assertEquals(expResult.getLongitude().get(), result.getLongitude().get(), 0.001);
+  }
+  
+  @Test
+  public void testGetCoordinate4() {
+    Coordinate point = new Coordinate(50, 10);
+    int bearing = 100;
+    double distanceInNM = 53.99568; // 100/3600;
+
+    Coordinate result = Coordinates.getCoordinate(point, bearing, distanceInNM);
+    Coordinate expResult = new Coordinate(49, 50, 8, 11, 22, 24);
+
+    assertEquals(expResult.getLatitude().get(), result.getLatitude().get(), 0.001);
+    assertEquals(expResult.getLongitude().get(), result.getLongitude().get(), 0.001);
   }
 
 }
