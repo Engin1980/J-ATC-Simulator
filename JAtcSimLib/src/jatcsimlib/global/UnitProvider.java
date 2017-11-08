57626e1ace307e221b72d01e0f3139e11cb40943
Engin1980 <engin@seznam.cz>
2017-11-08 17:38:49 +0100

    Fixed bug in UnitProvider

--- JAtcSimLib/src/jatcsimlib/global/UnitProvider.java
+++ JAtcSimLib/src/jatcsimlib/global/UnitProvider.java
@@ -7,24 +7,26 @@
 package jatcsimlib.global;
 
 /**
- *
  * @author Marek
  */
 public class UnitProvider {
-  public static double kmToNM(double value){
+  public static double kmToNM(double value) {
     return value * 0.539968;
   }
 
-  public static double mToNM(double value) {return this.kmToNM(value * 1000)}
+  public static double mToNM(double value) {
+    return UnitProvider.kmToNM(value * 1000);
+  }
 
   public static double nmToKm(double value) {
     return value * 1.85196;
   }
-  
-  public static double ftToNm(double value){
+
+  public static double ftToNm(double value) {
     return value / 6077.1;
   }
-  public static double nmToFt (double value){
+
+  public static double nmToFt(double value) {
     return value * 6076.1;
   }
 }
