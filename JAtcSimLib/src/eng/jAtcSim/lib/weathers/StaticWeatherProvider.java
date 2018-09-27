76dc9128f11679b1258289b2bc6455c60009ab30
Engin1980 <engin@seznam.cz>
2018-09-27 15:11:22 +0200

    BUG: Resolved different bugs in loading/saving according to the recent code changes.

--- JAtcSimLib/src/eng/jAtcSim/lib/weathers/StaticWeatherProvider.java
+++ JAtcSimLib/src/eng/jAtcSim/lib/weathers/StaticWeatherProvider.java
@@ -1,17 +1,21 @@
 package eng.jAtcSim.lib.weathers;
 
+import eng.eSystem.xmlSerialization.annotations.XmlIgnore;
+
 public class StaticWeatherProvider extends WeatherProvider {
 
-  private Weather initialWeather;
+  @XmlIgnore
+  private boolean wasReturned = false;
+  private Weather weather;
 
   public StaticWeatherProvider(Weather weather) {
-    this.initialWeather = weather;
+    this.weather = weather;
   }
 
   @Override
   public Weather tryGetNewWeather() {
-    Weather ret = initialWeather;
-    if (initialWeather != null) initialWeather = null;
-    return ret;
+    if (wasReturned) return null;
+    wasReturned = true;
+    return weather;
   }
 }
