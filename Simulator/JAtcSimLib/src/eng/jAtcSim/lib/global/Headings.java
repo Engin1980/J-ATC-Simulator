068420a028b241adf72004464f202395f577275a
Marek Vajgl <engin@seznam.cz>
2019-03-25 23:34:24 +0100

    UPD: Working on radial approach calculation.

--- Simulator/JAtcSimLib/src/eng/jAtcSim/lib/global/Headings.java
+++ Simulator/JAtcSimLib/src/eng/jAtcSim/lib/global/Headings.java
@@ -6,15 +6,15 @@
 
 package eng.jAtcSim.lib.global;
 
+import eng.eSystem.geo.Coordinate;
+import eng.eSystem.geo.Coordinates;
+import eng.eSystem.geometry2D.Line;
 import eng.jAtcSim.lib.speaking.fromAtc.commands.ChangeHeadingCommand;
 
 /**
  * @author Marek
  */
 public class Headings {
-
-  private Headings() {
-  }
 
   public static Object format(int heading) {
     return String.format("%03d", heading);
@@ -30,38 +30,13 @@ public class Headings {
   }
 
   public static double subtract(double a, double b) {
-    if (Math.abs(a-b) > 180){
+    if (Math.abs(a - b) > 180) {
       if (a < b)
         a += 360;
       else
         b += 360;
     }
     double ret = a - b;
-    return ret;
-  }
-
-  private static double getDifferenceShortestArc(double a, double b) {
-    double ret;
-
-    // ensure a <= b
-    if (a > b) {
-      double c = a;
-      a = b;
-      b = c;
-    }
-
-    ret = b - a;
-    if (ret > 180) {
-      // if is over 180, the other side might be closer
-      ret = (a + 360) - b;
-    }
-
-    return ret;
-  }
-
-  private static double getDifferenceAnyArc(double a, double b) {
-    double ret = a - b;
-    ret = to(ret);
     return ret;
   }
 
@@ -109,6 +84,68 @@ public class Headings {
   public static double getOpposite(double course) {
     double ret = course - 180;
     ret = Headings.to(ret);
+    return ret;
+  }
+
+  public static double getHeadingToFollowRadial(Coordinate currentPosition, Coordinate radialSourcePosition, double radial,
+                                                double maxHeadingDifference, double speedInKt) {
+    double ret = RadialCalculator.getHeadingToFollowRadial(currentPosition, radialSourcePosition, radial,
+        maxHeadingDifference, speedInKt);
+    return ret;
+  }
+
+  private static double getDifferenceShortestArc(double a, double b) {
+    double ret;
+
+    // ensure a <= b
+    if (a > b) {
+      double c = a;
+      a = b;
+      b = c;
+    }
+
+    ret = b - a;
+    if (ret > 180) {
+      // if is over 180, the other side might be closer
+      ret = (a + 360) - b;
+    }
+
+    return ret;
+  }
+
+  private static double getDifferenceAnyArc(double a, double b) {
+    double ret = a - b;
+    ret = to(ret);
+    return ret;
+  }
+
+  private Headings() {
+  }
+}
+
+class RadialCalculator {
+  public static double getHeadingToFollowRadial(Coordinate currentPosition, Coordinate fix, double radial,
+                                                double maxHeadingDifference, double speedInKt) {
+    Line line = getLine(fix, radial);
+    double targetHeading = Headings.getOpposite(radial);
+    double currentHeading = Coordinates.getBearing(currentPosition, fix);
+    double headingDifference = Headings.getDifference(currentHeading, targetHeading, true);
+    Coordinate turnFixPoint = evaluateTurnFixPoint(fix, radial, headingDifference >= 0, speedInKt);
+    double radialFromFixPoint = Coordinates.getBearing(turnFixPoint, currentPosition);
+    double 
+  }
+
+  private static Coordinate evaluateTurnFixPoint(Coordinate fix, double radial, boolean isOnLeft, double speedInKt) {
+    double flownDistance = speedInKt / 30;
+    double radius = flownDistance / 2 / Math.PI;
+    double directionFromFix = isOnLeft ? Headings.add(radial, 90) : Headings.add(radial, -90);
+    Coordinate ret = Coordinates.getCoordinate(fix, directionFromFix, radius);
+    return ret;
+  }
+
+  private static Line getLine(Coordinate a, double radial) {
+    Coordinate b = Coordinates.getCoordinate(a, radial, 10); // number is random
+    Line ret = new Line(a.getLatitude().get(), a.getLongitude().get(), b.getLatitude().get(), b.getLongitude().get());
     return ret;
   }
 }
