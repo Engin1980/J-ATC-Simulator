341700a6d280b8336c9e2fa8ebf1e3e3b4490138
Marek Vajgl <engin@seznam.cz>
2019-11-28 23:16:07 +0100

    Working on EDDH.ar.xml

--- Tools/JatcSimRouteTranslator/src/eng/jatcsim/routeTranslator/Main.java
+++ Tools/JatcSimRouteTranslator/src/eng/jatcsim/routeTranslator/Main.java
@@ -109,9 +109,13 @@ public class Main {
     maps.put("AD (\\S+)", "<after property=\"distance\" value=\":1\" />");
     maps.put("AR (\\S+)", "<after property=\"radial\" value=\":1\" />");
     maps.put("AN (\\S+)", "<after property=\"navaid\" value=\":1\" />");
+    maps.put("AL (\\d+)", "<altitudeRestriction restriction=\"above\" value=\":100\" />");
+    maps.put("AM (\\d+)", "<altitudeRestriction restriction=\"below\" value=\":100\" />");
+    maps.put("AE (\\d+)", "<altitudeRestriction restriction=\"exactly\" value=\":100\" />");
+    maps.put("AC", "<altitudeRestrictionClear />");
 
-    maps.put("DM (\\d{2,3})", "<altitude direction=\"descend\" value=\":1\" />");
-    maps.put("CM (\\d{2,3})", "<altitude direction=\"climb\" value=\":1\" />");
+    maps.put("DM (\\d{2,3})", "<altitude direction=\"descend\" value=\":100\" />");
+    maps.put("CM (\\d{2,3})", "<altitude direction=\"climb\" value=\":100\" />");
 
     maps.put("H (\\S+)", "<hold fix=\":1\" />");
     maps.put("T", "<then />");
