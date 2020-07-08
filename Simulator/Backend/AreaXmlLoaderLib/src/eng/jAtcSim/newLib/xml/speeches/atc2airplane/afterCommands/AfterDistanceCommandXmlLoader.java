c086203fc6adf085bd4f4e3438bdd21f3e770b97
Marek Vajgl <engin@seznam.cz>
2020-07-08 23:11:23 +0200

    Resolved issue after-distance loading.

--- Simulator/Backend/AreaXmlLoaderLib/src/eng/jAtcSim/newLib/xml/speeches/atc2airplane/afterCommands/AfterDistanceCommandXmlLoader.java
+++ Simulator/Backend/AreaXmlLoaderLib/src/eng/jAtcSim/newLib/xml/speeches/atc2airplane/afterCommands/AfterDistanceCommandXmlLoader.java
@@ -1,6 +1,8 @@
 package eng.jAtcSim.newLib.xml.speeches.atc2airplane.afterCommands;
 
 import eng.eSystem.eXml.XElement;
+import eng.eSystem.utilites.RegexUtils;
+import eng.eSystem.validation.EAssert;
 import eng.jAtcSim.newLib.shared.enums.AboveBelowExactly;
 import eng.jAtcSim.newLib.shared.xml.IXmlLoader;
 import eng.jAtcSim.newLib.shared.xml.SmartXmlLoaderUtils;
@@ -10,9 +12,20 @@ public class AfterDistanceCommandXmlLoader implements IXmlLoader<AfterDistanceCo
 
   @Override
   public AfterDistanceCommand load(XElement element) {
-    String navaidName = Shared.loadNavaidName(element);
-    double distance = SmartXmlLoaderUtils.loadDouble(element, "distance");
-    AboveBelowExactly extension = Shared.loadAfterValuePosition(element);
+    EAssert.isTrue("distance".equals(element.getAttribute("property")));
+
+    String val = element.getAttribute("value");
+    String [] pts = RegexUtils.extractGroups(val, "([A-Z]+)/(\\d+(\\.\\d+)?)([\\+\\-])?");
+
+    String navaidName = pts[1];
+    double distance = Double.parseDouble(pts[2]);
+    AboveBelowExactly extension = AboveBelowExactly.exactly;
+    if (pts.length == 4)
+      if (pts[3].equals("-"))
+        extension = AboveBelowExactly.below;
+      else if (pts[3].equals("+"))
+        extension = AboveBelowExactly.above;
+
     AfterDistanceCommand ret = AfterDistanceCommand.create(navaidName, distance, extension);
     return ret;
   }
