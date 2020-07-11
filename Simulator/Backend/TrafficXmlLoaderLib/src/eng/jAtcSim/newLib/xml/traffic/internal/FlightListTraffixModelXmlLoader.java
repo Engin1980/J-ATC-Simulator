825de7fafbe94adb6700377fc8d378ae8f9d1f3a
Marek Vajgl <engin@seznam.cz>
2020-07-11 22:33:03 +0200

    Resolved bug in traffic analyse in flight-list traffic.

--- Simulator/Backend/TrafficXmlLoaderLib/src/eng/jAtcSim/newLib/xml/traffic/internal/FlightListTraffixModelXmlLoader.java
+++ Simulator/Backend/TrafficXmlLoaderLib/src/eng/jAtcSim/newLib/xml/traffic/internal/FlightListTraffixModelXmlLoader.java
@@ -4,6 +4,7 @@ import eng.eSystem.collections.*;
 import eng.eSystem.eXml.XElement;
 import eng.eSystem.geo.Coordinate;
 import eng.jAtcSim.newLib.shared.Callsign;
+import eng.jAtcSim.newLib.shared.xml.IXmlLogable;
 import eng.jAtcSim.newLib.shared.xml.SmartXmlLoaderUtils;
 import eng.jAtcSim.newLib.traffic.models.FlightListTrafficModel;
 import eng.jAtcSim.newLib.traffic.movementTemplating.MovementTemplate;
@@ -11,9 +12,10 @@ import eng.jAtcSim.newLib.traffic.movementTemplating.MovementTemplate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
-public class FlightListTraffixModelXmlLoader {
+public class FlightListTraffixModelXmlLoader implements IXmlLogable {
   public FlightListTrafficModel load(XElement source) {
     SmartXmlLoaderUtils.setContext(source);
+    log(0, "Xml-loading flight list traffic");
 
     IList<FlightListTrafficModel.Flight> flights = new EList<>();
     SmartXmlLoaderUtils.loadList(source.getChildren("flight"),
@@ -25,12 +27,14 @@ public class FlightListTraffixModelXmlLoader {
   }
 
   private FlightListTrafficModel.Flight loadFlight(XElement source) {
+    log(1, "loading flight");
     SmartXmlLoaderUtils.setContext(source);
+    String callsignS = SmartXmlLoaderUtils.loadString("callsign");
+    log(1, "... flight '%s'", callsignS);
     String timeS = SmartXmlLoaderUtils.loadString("time");
     LocalTime time = LocalTime.parse(
         timeS,
-        DateTimeFormatter.ofPattern("HH:mm"));
-    String callsignS = SmartXmlLoaderUtils.loadString("callsign");
+        DateTimeFormatter.ofPattern("H:mm"));
     Callsign callsign = new Callsign(callsignS);
     MovementTemplate.eKind kind = SmartXmlLoaderUtils.loadEnum("kind", MovementTemplate.eKind.class);
     Integer heading = SmartXmlLoaderUtils.loadInteger("heading", null);
