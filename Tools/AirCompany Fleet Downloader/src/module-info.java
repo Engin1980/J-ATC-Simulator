6f983ad1a4361654e09e89a0bcc61e738e003b26
Marek Vajgl <engin@seznam.cz>
2020-07-19 01:30:14 +0200

    Resolved issue about references and dependencies.

--- Tools/AirCompany Fleet Downloader/src/module-info.java
+++ Tools/AirCompany Fleet Downloader/src/module-info.java
@@ -1,5 +1,5 @@
-module eng.jAtcSim.tools.AirCompanyFleetDownloader {
-  requires eng.eXmlSerialization;
+module AirCompanyFleetDownloader {
   requires eng.eSystem;
+  requires eng.eXmlSerialization;
   opens eng.airCompanyFleetDownloader to eng.eXmlSerialization;
 }
