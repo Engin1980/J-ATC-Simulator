84eafae453d39ca444d3c075474c9a3c819d3536
Marek Vajgl <engin@seznam.cz>
2020-07-20 00:49:54 +0200

    Reimplementing AirCompany Fleet Downloader

--- Tools/AirCompany Fleet Downloader/src/eng/airCompanyFleetDownloader/Program.java
+++ Tools/AirCompany Fleet Downloader/src/eng/airCompanyFleetDownloader/Program.java
@@ -1,25 +1,27 @@
 package eng.airCompanyFleetDownloader;
 
 import eng.eSystem.EStringBuilder;
-import eng.eSystem.collections.EDistinctList;
-import eng.eSystem.collections.EList;
-import eng.eSystem.collections.EMap;
-import eng.eSystem.collections.IList;
+import eng.eSystem.collections.*;
 import eng.eSystem.eXml.XDocument;
 import eng.eSystem.eXml.XElement;
 import eng.eSystem.exceptions.EApplicationException;
 import eng.eSystem.exceptions.ERuntimeException;
 import eng.eSystem.exceptions.EXmlException;
 import eng.eSystem.utilites.ExceptionUtils;
+import eng.eXmlSerialization.StoreTypeMode;
 import eng.eXmlSerialization.XmlSerializer;
 import eng.eXmlSerialization.XmlSettings;
 import eng.eXmlSerialization.common.Log;
+import eng.eXmlSerialization.meta.XmlRule;
 
 import java.io.*;
 import java.net.HttpURLConnection;
+import java.net.SocketTimeoutException;
 import java.net.URL;
+import java.nio.channels.Pipe;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -40,22 +42,48 @@ public class Program {
     }
 
     //tmp();
-    //downloadAirlines();
-    downloadAirlinesFleets();
-    convertToFleets();
+    downloadAirlines();
+    //downloadAirlinesFleets();
+    //convertToFleets();
   }
 
-  private static void convertToFleets() throws EXmlException {
-    XmlSerializer ser = new XmlSerializer();
-    IList<AirlineInfo> lst = deserializeFromDocument(ser, getAirlinesListFile().toString(), EList.class);
+  private static void convertToFleets(){
+    System.out.println("Phase 3 - converting to fleets xml file");
 
+    XmlSerializer ser = getSerializer();
+    IList<AirlineInfo> lst = deserializeFromDocument(ser, getAirlinesListFile().toString(), EList.class);
+    System.out.println("\t found " + lst.count() + " airlines");
+    lst = lst.where(q -> q.isDecoded());
+    System.out.println("\t found " + lst.count() + " decoded");
+    lst = lst.where(q -> q.isActive());
+    System.out.println("\t found " + lst.count() + " active");
     lst = lst.where(q -> q.getCodes().count(p -> p.length() == 3) == 1);
+    System.out.println("\t found " + lst.count() + " airlines with exactly one ICAO code");
+    IMap<String, IList<AirlineInfo>> map = lst.groupBy(q->q.getCodes().getFirst(p->p.length() == 3));
+    System.out.println("\t found " + map.getKeys().count() + " unequivocal ICAO codes");
+    System.out.println("\t duplicate ICAOs:");
+    for (Map.Entry<String, IList<AirlineInfo>> entry : map.where(q -> q.getValue().count() > 1)) {
+      System.out.println("\t\t" + entry.getKey() + " - " + entry.getValue().count());
+      System.out.println("\t\t used will be: " + entry.getValue().get(0).getName());
+    }
+
+    ISet<String> allUsedTypes = lst.selectMany(q->q.getFleet().getKeys().toList()).toSet();
+    ISet<String> unknownTypes = allUsedTypes.minus(typeMap.getKeys());
+    if (unknownTypes.isEmpty() == false){
+      System.out.println("\t unknown plane types:");
+      for (String unknownType : unknownTypes) {
+        System.out.println("\t\t" + unknownType);
+      }
+    }
+
+    System.out.println("\t creating xml fleet file");
 
     EStringBuilder sb = new EStringBuilder();
     sb.appendLine("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
     sb.appendLine("<fleets>");
 
-    for (AirlineInfo ai : lst) {
+    for (Map.Entry<String, IList<AirlineInfo>> entry : map) {
+      AirlineInfo ai = entry.getValue().getFirst();
       sb.appendFormatLine("  <company icao=\"%s\" name=\"%s\">", ai.getCodes().getFirst(q -> q.length() == 3), ai.getName());
       sb.appendLine("    <types>");
 
@@ -77,10 +105,10 @@ public class Program {
       bw.write(sb.toString());
       bw.flush();
     } catch (IOException ex) {
-
+      System.out.println("Failed to write fleets file into " + getFleetXmlFile().toString() + ". " + ExceptionUtils.toFullString(ex));
     }
 
-    System.out.println("Done.");
+    System.out.println("Phase 3 - done");
   }
 
   private static String decodeNextPageUrl(String url) {
@@ -161,7 +189,7 @@ public class Program {
     p = Pattern.compile(typeRegex);
     m = p.matcher(cnt);
     while (m.find()) {
-      String type = m.group(1);
+      String type = m.group(1).trim();
       String countS = m.group(2);
       if (countS != null) countS = countS.trim();
       if (countS != null && countS.length() > 0) {
@@ -179,8 +207,10 @@ public class Program {
 
   private static void downloadAirlines() throws EXmlException {
 
+    System.out.println("Phase 1 - downloading airlines");
+
     IList<AirlineInfo> lst = new EList<>();
-    XmlSerializer ser = new XmlSerializer();
+    XmlSerializer ser = getSerializer();
 
     String base = "https://www.airfleets.net/recherche/list-airline-%s.htm";
     for (int i = 'a'; i <= 'z'; i++) {
@@ -194,6 +224,8 @@ public class Program {
 
     lst = lst.where(q -> q.isActive());
     serializeToDocument(ser, lst, getAirlinesListFile().toString());
+
+    System.out.println("Phase 1 - done");
   }
 
   private static IList<AirlineInfo> downloadAirlinesByLetter(String url) {
@@ -211,14 +243,14 @@ public class Program {
       String an = m.group(2);
       boolean isActive = m.group(4) == null;
 
-      au = "http://www.airfleets.net/" + au.substring(3);
+      au = "https://www.airfleets.net/" + au.substring(3);
 
       AirlineInfo ai = new AirlineInfo(an, au, isActive);
       ret.add(ai);
     }
 
     String nextPageUrl = decodeNextPageUrl(url);
-    String nextPageUrlPart = nextPageUrl.substring(35);
+    String nextPageUrlPart = nextPageUrl.substring(36);
     if (cnt.contains(nextPageUrlPart)) {
       IList<AirlineInfo> tmp = downloadAirlinesByLetter(nextPageUrl);
       ret.add(tmp);
@@ -228,10 +260,12 @@ public class Program {
   }
 
   private static void downloadAirlinesFleets() {
+
+    System.out.println("Phase 2 - downloading airlines");
+
     IList<AirlineInfo> src;
-    XmlSettings sett = new XmlSettings();
-    sett.getLog().getOnLog().add(new Log.DefaultOnLogHandler());
-    XmlSerializer ser = new XmlSerializer(sett);
+    XmlSerializer ser = getSerializer();
+
     src = (IList<AirlineInfo>) deserializeFromDocument(ser, getAirlinesListFile().toString(), EList.class);
     int savedCount = 0;
     int saveStep = 10;
@@ -240,7 +274,7 @@ public class Program {
     int undecodedCount = src.count(q -> q.isDecoded() == false);
     System.out.println("Undecoded airlines: " + undecodedCount);
 
-    System.out.println("Checking duplicit");
+    System.out.println("Checking duplicates");
     IList<AirlineInfo> toRem = new EList<>();
     for (AirlineInfo a : src) {
       for (AirlineInfo b : toRem) {
@@ -249,19 +283,23 @@ public class Program {
         }
       }
     }
-    System.out.println("Found " + toRem.size() + " duplicit records, removing...");
+    System.out.println("Found " + toRem.size() + " duplicate records, removing...");
     for (AirlineInfo b : toRem) {
       src.remove(b);
     }
 
     for (AirlineInfo airline : src) {
-      if (airline.isDecoded()) continue;
+      System.out.println("Processing airline " + airline.getName());
+      if (airline.isDecoded()) {
+        System.out.println("\t already decoded, skipping");
+        continue;
+      }
       if (airline.isActive() == false) {
+        System.out.println("\t not active, skipping");
         airline.setDecoded(true);
         continue;
       }
 
-      System.out.println("Processing airline " + airline.getName());
 
       try {
         downloadAirlineFleet(airline);
@@ -280,6 +318,8 @@ public class Program {
     }
 
     serializeToDocument(ser, src, getAirlinesListFile().toString());
+
+    System.out.println("Phase 2 - done");
   }
 
   private static Path getAirlinesListFile() {
@@ -299,6 +339,27 @@ public class Program {
   private static Path getFleetXmlFile() {
     Path ret =
         Paths.get(path.toAbsolutePath().toString(), "fleets.xml");
+    return ret;
+  }
+
+  private static XmlSerializer getSerializer() {
+    XmlSettings sett = new XmlSettings();
+    sett.setStoreTypeMode(StoreTypeMode.dontStore);
+    sett.setStoreTypeMode(StoreTypeMode.dontStore);
+    sett.getLog().getOnLog().add(new Log.DefaultOnLogHandler());
+    sett.getMeta().forClass(EList.class).getRule().with(0, new XmlRule(null, AirlineInfo.class));
+
+    sett.getMeta().forClass(AirlineInfo.class).forField("fleet")
+        .getRules().add(
+        new XmlRule(null, null)
+            .with(0, new XmlRule(null, String.class))
+            .with(1, new XmlRule(null, Integer.class)));
+    sett.getMeta().forClass(AirlineInfo.class).forField("codes")
+        .getRules().add(
+            new XmlRule(null, null)
+            .with(0, new XmlRule(null, String.class))
+    );
+    XmlSerializer ret = new XmlSerializer(sett);
     return ret;
   }
 
@@ -352,12 +413,14 @@ public class Program {
     typeMap.set("Boeing 717", "B717");
     typeMap.set("Boeing 737", "B737");
     typeMap.set("Boeing 737 Next Gen", "B737NG");
+    typeMap.set("Boeing 737 NG / Max", "B737NG");
     typeMap.set("Boeing 747", "B747");
     typeMap.set("Boeing 757", "B757");
     typeMap.set("Boeing 767", "B767");
     typeMap.set("Boeing 777", "B777");
     typeMap.set("Boeing 787", "B787");
     typeMap.set("Bombardier C-Series", "BCS1");
+    typeMap.set("Airbus A220", "BCS1");
     typeMap.set("Canadair Regional Jet", "CRJ");
     typeMap.set("Comac ARJ21", "AJ21");
     typeMap.set("Dash 8", "DH8A");
@@ -369,9 +432,12 @@ public class Program {
     typeMap.set("Fokker 70/100", "F70");
     typeMap.set("Iliouchine Il-96", "IL96");
     typeMap.set("Lockheed L-1011 TriStar", "L101");
+    typeMap.set("McDonnell DC-10", "DC10");
     typeMap.set("McDonnell Douglas DC-10", "DC10");
+    typeMap.set("McDonnell MD-11", "MD11");
     typeMap.set("McDonnell Douglas MD-11", "MD11");
     typeMap.set("McDonnell Douglas MD-80/90", "MD90");
+    typeMap.set("McDonnell MD-80/90", "MD90");
     typeMap.set("Saab 2000", "SB20");
     typeMap.set("Saab 340", "SF34");
     typeMap.set("Sukhoi SuperJet 100", "SU95");
