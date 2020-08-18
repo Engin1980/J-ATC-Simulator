ef6caab9631d4dac055ca380487fa070cc8373b5
Marek Vajgl <engin@seznam.cz>
2020-08-19 00:35:43 +0200

    Rewritten Message listening to new format.

--- Simulator/Backend/MessagingLib/src/eng/jAtcSim/newLib/messaging/Messenger.java
+++ Simulator/Backend/MessagingLib/src/eng/jAtcSim/newLib/messaging/Messenger.java
@@ -1,42 +1,66 @@
 package eng.jAtcSim.newLib.messaging;
 
-import eng.eSystem.collections.EList;
-import eng.eSystem.collections.IList;
+import eng.eSystem.collections.*;
 import eng.eSystem.exceptions.EApplicationException;
 import eng.eSystem.validation.EAssert;
 
-import static eng.eSystem.utilites.FunctionShortcuts.sf;
-
 public class Messenger {
 
-  public static class ListenerInfo {
-    public final Object key;
-    public final Participant messageTarget;
-    public final IList<Message> queue;
+  public static class ListenerAim {
+    private final eListenerDirection direction;
+    private final Participant participant;
 
-    public ListenerInfo(Object key, Participant messageTarget) {
-      this.key = key;
-      this.messageTarget = messageTarget;
-      this.queue = new EList<>();
+    public ListenerAim(Participant participant, eListenerDirection direction) {
+      this.participant = participant;
+      this.direction = direction;
+    }
+
+    public eListenerDirection getDirection() {
+      return direction;
+    }
+
+    public Participant getParticipant() {
+      return participant;
     }
   }
+
+  public static class ListenerInfo {
+    private final IList<ListenerAim> aims = new EList<>();
+    private final IList<Message> queue = new EList<>();
+
+    public ListenerInfo(ListenerAim... aims) {
+      this.aims.addMany(aims);
+    }
+
+    public IReadOnlyList<ListenerAim> getAims() {
+      return aims;
+    }
+  }
+
+  public enum eListenerDirection {
+    sender,
+    receiver
+  }
+
   public static final Participant SYSTEM = Participant.createSystem();
-  private IList<ListenerInfo> listeners = new EList<>();
+  private IMap<Object, ListenerInfo> listeners = new EMap<>();
   private final MessengerLog messengerLog = new MessengerLog("messenger_log.txt");
 
-  public IList<Message> getMessagesByListener(Participant listener, boolean deleteRetrieved) {
-    ListenerInfo li;
+  public IList<Message> getMessagesByListener(Object listener, boolean deleteRetrieved) {
+    EAssert.Argument.isNotNull(listener, "listener");
+
+    ListenerInfo lis;
     IList<Message> ret;
 
     synchronized (this) {
       try {
-        li = listeners.getFirst(q -> q.key.equals(listener));
+        lis = listeners.get(listener);
       } catch (Exception ex) {
         throw new EApplicationException("Listener " + listener.toString() + " has not been registered.");
       }
-      ret = new EList(li.queue);
+      ret = new EList<>(lis.queue);
       if (deleteRetrieved) {
-        li.queue.clear();
+        lis.queue.clear();
       }
     }
 
@@ -45,41 +69,41 @@ public class Messenger {
     return ret;
   }
 
-  public void registerCustomListener(Object key, Participant messageTarget) {
-    EAssert.Argument.isNotNull(key, "key");
-    EAssert.Argument.isNotNull(messageTarget, "messageTarget");
-    if (listeners.isAny(q -> q.key.equals(key)))
-      throw new EApplicationException("Listener " + key + " already registered.");
-
-    ListenerInfo li = new ListenerInfo(key, messageTarget);
-    this.listeners.add(li);
-
-    messengerLog.recordRegistratin(MessengerLog.eRegistrationAction.REGISTER, key);
+  public void registerListener(Participant listener) {
+    ListenerAim listenerAim = new ListenerAim(listener, eListenerDirection.receiver);
+    registerListener(listener, listenerAim);
   }
 
-  public void registerListener(Participant listener) {
-    registerCustomListener(listener, listener);
+  public void registerListener(Object listener, ListenerAim... aims) {
+    EAssert.Argument.isNotNull(listener, "listener");
+    EAssert.Argument.isNotNull(aims, "aims");
+    EAssert.Argument.isTrue(aims.length > 0, "Aims must be non empty");
+    EAssert.Argument.isFalse(this.listeners.containsKey(listener), "Listener is already registered.");
+
+    ListenerInfo listenerInfo = new ListenerInfo(aims);
+    listeners.set(listener, listenerInfo);
+
     messengerLog.recordRegistratin(MessengerLog.eRegistrationAction.REGISTER, listener);
   }
 
   public void send(Message msg) {
     synchronized (this) {
-      IList<ListenerInfo> tmp = listeners.where(q -> q.messageTarget.equals(msg.getTarget()));
-      tmp.forEach(q -> q.queue.add(msg));
+      for (ListenerInfo listenerInfo : listeners.getValues()) {
+        if (listenerInfo.getAims().isAny(q ->
+            (q.direction == eListenerDirection.sender && q.participant.equals(msg.getSource()))
+            ||
+            (q.direction == eListenerDirection.receiver && q.participant.equals(msg.getTarget())))) {
+          listenerInfo.queue.add(msg);
+        }
+      }
       messengerLog.recordMessage(MessengerLog.eMessageAction.ADD, msg);
     }
   }
 
-  public void unregisterCustomListener(Object key) {
-    EAssert.Argument.isNotNull(key, "key");
-    ListenerInfo li = listeners.tryGetFirst(q -> q.key.equals(key));
-    if (li == null)
-      throw new EApplicationException(sf("There is no listener object '%s'.", key));
+  public void unregisterListener(Object listener) {
+    EAssert.Argument.isNotNull(listener, "listener");
+    EAssert.Argument.isTrue(listeners.containsKey(listener), "Listener is not registered.");
 
-    listeners.remove(li);
-  }
-
-  public void unregisterListener(Participant listener) {
-    unregisterCustomListener(listener);
+    this.listeners.remove(listener);
   }
 }
